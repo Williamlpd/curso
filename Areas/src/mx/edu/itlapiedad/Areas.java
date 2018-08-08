6e3ef5604cd3ab0d39e60e8d13f470fc4e1bea75
PC_05 <PC_05@DESKTOP-B4OTD69.lan>
2018-08-08 09:51:13 -0500

    modifique el archivo

--- Areas/src/mx/edu/itlapiedad/Areas.java
+++ Areas/src/mx/edu/itlapiedad/Areas.java
@@ -5,7 +5,7 @@ public class Areas {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Triangulo t=new Triangulo(10, 20);
+		Triangulo t=new Triangulo(5, 3);
 		System.out.println(t.getArea());
 	}
 
