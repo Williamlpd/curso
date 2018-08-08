788887c6d3d8bf1e0bacc1f23f9c80634edd6be5
PC_05 <PC_05@DESKTOP-B4OTD69.lan>
2018-08-08 10:22:41 -0500

    Se agrero clase Rectangulo

--- Areas/src/mx/edu/itlapiedad/Rectangulo.java
+++ Areas/src/mx/edu/itlapiedad/Rectangulo.java
@@ -1,5 +1,37 @@
 package mx.edu.itlapiedad;
 
 public class Rectangulo {
+	
+	private int base, altura;
+
+	public Rectangulo(int base, int altura) {
+		super();
+		this.base = base;
+		this.altura = altura;
+	}
+
+	public int getBase() {
+		return base;
+	}
+
+	public void setBase(int base) {
+		this.base = base;
+	}
+
+	public int getAltura() {
+		return altura;
+	}
+
+	public void setAltura(int altura) {
+		this.altura = altura;
+	}
+	
+	
+	public float Area() {
+		
+		return base*altura;
+		
+	}
+	
 
 }
